538940804cb02f98295d35019416b0938ae5584b
stevepae <stevepaemusic@gmail.com>
2021-06-11 17:49:36 -0400

    add env

--- src/services/movies-api.js
+++ src/services/movies-api.js
@@ -1,13 +1,7 @@
-
 function getMovies() {
-
-    return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=US`).then(res => res.json());
+  return fetch(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${REACT_APP_API_KEY}&language=en-US&page=1&region=US`
+  ).then((res) => res.json());
 }
 
-
-const API_KEY = '01d8cda98918b4254748ca838842a38b'
-
-// https://api.themoviedb.org/3/movie/now_playing?api_key=01d8cda98918b4254748ca838842a38b&language=en-US&page=1&region=US
-
-
-export default getMovies
+export default getMovies;
