f33b2f51230b64758d5c7a2e419a65918449a332
stevepae <stevepaemusic@gmail.com>
2021-06-11 18:26:08 -0400

    updated env

--- src/services/movies-api.js
+++ src/services/movies-api.js
@@ -1,6 +1,6 @@
 function getMovies() {
   return fetch(
-    `https://api.themoviedb.org/3/movie/now_playing?api_key=${REACT_APP_API_KEY}&language=en-US&page=1&region=US`
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&region=US`
   ).then((res) => res.json());
 }
 
